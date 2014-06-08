fc4b686d5765b66bf7ba28da3cc830cb5538b90d
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-06-08 13:45:13 +0200

    Code templates for JUnit4 class and method

---
+++ settings/fileTemplates/code/JUnit4 Test Class.java
@@ -0,0 +1,7 @@
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+#parse("File Header")
+public class ${NAME} {
+  ${BODY}
+}
---
+++ settings/fileTemplates/code/JUnit4 Test Method.java
@@ -0,0 +1,8 @@
+@org.junit.Test
+public void should${NAME}() throws Exception {
+    // given:
+    ${BODY}
+    // when:
+    
+    // then:
+}
