c05b142927ebfde6dc265795eecaa3ef19b11d4a
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-01-21 13:23:39 +0100

    Adding CompileStatic annotation to new Groovy classes by default

--- settings/fileTemplates/j2ee/GroovyClass.groovy
+++ settings/fileTemplates/j2ee/GroovyClass.groovy
@@ -1,9 +1,11 @@
 #if (${PACKAGE_NAME} && $PACKAGE_NAME != "" )package ${PACKAGE_NAME}
 #end
 
+import groovy.transform.CompileStatic
 import groovy.transform.PackageScope
 
 #parse("File Header.java")
 @PackageScope
+@CompileStatic
 class ${NAME} {
 }
